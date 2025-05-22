ea8954e79d6af0e6a5021ac2d8941da1e2c7e04c
Szymon Kubica <szymon.kubica@srccode.pl>
2025-05-22 16:10:49 +0200

    cw 2

--- Lista1R/R/cw2.R
+++ Lista1R/R/cw2.R
@@ -1,28 +1,63 @@
-wspolne <- function(a,b)
+#' @author Szymon Kubica 264068
+#'
+#' @title Część wspólna dwóch multizbiorów
+#'
+#' @description
+#' Funkcja \code{wspolne} zwraca multizbiór bedący częścią wspólną
+#' dwóch podanych wektorów \code{x} i \code{y}.
+#'
+#' @param x \cr
+#'  Pierwszy multizbiór (wektor liczb, tekstów itp.).
+#' @param y \cr
+#'  Drugi multizbiór (wektor tego samego typu co \code{x})
+#'
+#' @return Ten sam typ co \code{x,y}: wektor zawierający elementy
+#' wspólne, powtórzone minimalną liczbę razy. Jeżeli nie ma wspólnych
+#' elementów, zwraca wektor długośći 0.
+#'
+#' @examples
+#' wspolne(c(1,2,3), c(2,2,4)) # -> c(2)
+#' wspolne(c("a","b","a"), c("b","b")) # -> "b"
+#'
+#' @export
+wspolne <- function(x,y)
 {
-  if(is.list(a) == FALSE || is.list(b) == FALSE)
+  # Oba argumenty muszą byc podane
+  if(missing(x) || missing(y))
   {
-     return(FALSE)
+    stop("Oba argumenty muszą być podane")
   }
-  
-  rV <- list()
-  
-  for (i in seq_along(a))
+  # Muszą być to atomowe wektory
+  if(!is.atomic(x) || !is.atomic(y))
   {
-    for (j in seq_along(b))
-    {
-      if(a[[i]] == b[[j]])
-      {
-        rV[length(rV)] <- a[i]
-      }
-    }
+    stop("Oba argumenty muszą być atomowymi wektorami")
   }
-  
-  return(unique(rV))
+  # Typy muszą się zgadzać
+  if (typeof(x) != typeof(y)) {
+    stop("Oba argumenty muszą być tego samego typu")
+  }
+
+  result <- intersect(x, y)
+
+  return(result)
 }
 
-a <- list(1,2,1,4)
-b <- list(2,5,4,1)
 
-part <- wspolne(a,b)
-print(part)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
